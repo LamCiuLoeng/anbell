f7c7c20d0029058dd94dba8b2928010e058f9bca
ecgol <ecgol@qq.com>
2014-07-21 12:15:05 +0800

    删除多余文件

--- anbels/Common/Conf/config.php
+++ anbels/Common/Conf/config.php
@@ -2,11 +2,11 @@
 return array(
     //'配置项'=>'配置值'
      'db_type'  => 'mysql',
-     'db_host'  => 'localhost',
+     'db_host'  => '113.105.167.196',
      'db_port'  => '3306',
-     'db_name'  => 'anbels',
-     'db_user'  => 'root',
-     'db_pwd'  => 'root',
+     'db_name'  => 'my10383',
+     'db_user'  => 'my10383',
+     'db_pwd'  => '5tgbnhy6',
      'DB_CHARSET'=> 'utf8',
      
 
