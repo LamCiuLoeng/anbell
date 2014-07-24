489f55c18e81656bf723e5c209d6a0466b7cb0db
CL.Lam <lamciuloeng>
2014-07-24 23:41:16 +0800

    add the auth login to the basic class

--- anbels/Admin/Controller/BaseController.class.php
+++ anbels/Admin/Controller/BaseController.class.php
@@ -2,9 +2,10 @@
 namespace Admin\Controller;
 use Think\Controller;
 class BaseController extends Controller {
-    public function _before_index(){
+    public function _initialize(){
         if(!session('?login')){
             $this->redirect('Auth/login',NULL,0);
         }
     }
 }
+        
