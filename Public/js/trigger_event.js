a4bb29b4f00fb1afff9ea9989062ba4686ea58b5
ecgol <ecgol@qq.com>
2014-07-31 01:09:25 +0800

    题库 学校 班级 分页

--- Public/js/trigger_event.js
+++ Public/js/trigger_event.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	$('.current').parent().addClass('div_page_content')
+	
+	$.get(request_category_URL, function(data){
+		var htm=''
+		for(var i=0;i<data.length;i++)
+		{
+			htm+= ('<option value='+data[i].id+'>'+data[i].name+'</option>')
+		}
+		$('#select_category').append(htm)
+
+	})
+	
 	var i = 0;
 	$(".odd_tr").each(function(){
 		if(i%2==0){
@@ -51,6 +63,54 @@ $(document).ready(function(){
 		
 	})
 	
+	$('#question_send_btn').click(function(){
+		var question_name=$('input[name=question_name]');
+		var correct_answer=$('input[name=correct_answer]');
+		var answer01=$('input[name=answer01]');
+		var answer_content01=$('input[name=answer_content01]');
+		var answer02=$('input[name=answer02]');
+		var answer_content02=$('input[name=answer_content02]');
+		
+		if(question_name.val()==''){
+		alert('题目不能为空！');
+		question_name.focus();
+		return;
+		}
+		
+		if(correct_answer.val()==''){
+		alert('正确答案不能为空！');
+		correct_answer.focus();
+		return;
+		}
+		
+		if(answer01.val()==''){
+		alert('答案不能为空！');
+		answer01.focus();
+		return;
+		}
+		
+		if(answer_content01.val()==''){
+		alert('答案内容不能为空！');
+		answer_content01.focus();
+		return;
+		}
+		
+		if(answer02.val()==''){
+		alert('答案不能为空！');
+		answer02.focus();
+		return;
+		}
+		
+		if(answer_content02.val()==''){
+		alert('答案内容不能为空！');
+		answer_content02.focus();
+		return;
+		}
+		
+		$('#form1').submit();
+		
+	})
+	
 	$('#reset_btn').click(function(){
 		$('#form1')[0].reset();   
 	})
@@ -75,6 +135,18 @@ $(document).ready(function(){
 	
 	$('#class_list_delete').click(function(){
 		$('#form1').attr('action',class_list_delete_URL);
+		$('#form1').submit();
+		
+	})
+	
+	$('#question_list_edit').click(function(){
+		$('#form1').attr('action',question_edit_URL);
+		$('#form1').submit();
+		
+	})
+	
+	$('#question_list_delete').click(function(){
+		$('#form1').attr('action',question_list_delete_URL);
 		$('#form1').submit();
 		
 	})
