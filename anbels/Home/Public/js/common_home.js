920cc6bd0ef7d13f4b9fe75d4d5563b19e5a43d0
ecgol <ecgol@qq.com>
2014-08-22 01:52:07 +0800

    前台页面修改

--- anbels/Home/Public/js/common_home.js
+++ anbels/Home/Public/js/common_home.js
@@ -3,6 +3,25 @@ $(document).ready(function(){
 		$('.sub_content').height(500)
 	}
 	
+	var folder_path = '/anbels/Home/Public/images/';
+	function switch_image(current_img,replace_img) 
+	{
+		$("img[src='"+folder_path+current_img+"']").hover(
+		  function () {
+			$(this).attr("src",folder_path+replace_img); 
+		  },
+		  function () {
+			$(this).attr("src",folder_path+current_img); 
+		  }
+		); 
+	}
+	
+	switch_image('home_06.jpg','home_06_h.jpg');
+	switch_image('home_07.jpg','home_07_h.jpg');
+	switch_image('home_08.jpg','home_08_h.jpg');
+	switch_image('home_09.jpg','home_09_h.jpg');
+
+	
 	$("img[src='/anbels/Home/Public/images/guanyuanbeier_06.jpg']").hover(
 	  function () {
 		$(this).attr("src","/anbels/Home/Public/images/guanyuanbeier_06_h.jpg"); 
